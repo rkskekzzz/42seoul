21f24ab5c03868b0fd663eb4782258eaaa78da73
Suhyoung Shin <suhshin@c4r3s8.42seoul.kr>
2021-03-28 17:29:40 +0900

    miniRT

--- Circle-02/miniRT/minirt.c
+++ Circle-02/miniRT/minirt.c
@@ -1,13 +1,39 @@
 #include <unistd.h>
 #include <mlx.h>
 
+typedef struct s_data
+{
+	void 	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}		t_data;
+
+void			my_mlx_pixel_put(t_data *data, int x, int y, int color)
+{
+	char	*dst;
+
+	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+	*(unsigned int*)dst = color;
+}
+
 int main()
 {
 	void	*mlx_ptr;
-	void	*win_ptr; 
-	
-	mlx_ptr = mlx_init();	
-	win_ptr = mlx_new_window(mlx_ptr, 500, 500, "Hellow World!"); 
+	void	*win_ptr;
+	t_data	image;
+
+	mlx_ptr = mlx_init();
+	win_ptr = mlx_new_window(mlx_ptr, 1920, 1080, "Hellow World!");
+	image.img = mlx_new_image(mlx_ptr, 1920, 1080); // 이미지 객체 생성
+	image.addr = mlx_get_data_addr(image.img, &image.bits_per_pixel, &image.line_length, &image.endian); // 이미지 주소 할당
+	for (int i = 0 ; i < 200 ; i++)
+	{
+		mlx_pixel_put (mlx_ptr, win_ptr, i, i, 0x00FF0000);
+		//my_mlx_pixel_put(&image, i, i, 0x00FF0000);
+	}
+	mlx_put_image_to_window(mlx_ptr, win_ptr, image.img, 0, 0);
 	mlx_loop(mlx_ptr);
 	return (0);
 }
