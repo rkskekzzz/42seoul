0853ec793e6d6b722f5c4f8ae0c3a8f50ee38ff2
Suhyoung Shin <suhshin@c3r1s5.42seoul.kr>
2021-03-29 12:17:03 +0900

    hooking

--- Circle-02/miniRT/minirt.c
+++ Circle-02/miniRT/minirt.c
@@ -1,5 +1,7 @@
 #include <unistd.h>
 #include <mlx.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 typedef struct s_data
 {
@@ -18,7 +20,7 @@ void			my_mlx_pixel_put(t_data *data, int x, int y, int color)
 	*(unsigned int*)dst = color;
 }
 
-int main()
+int prtimage()
 {
 	void	*mlx_ptr;
 	void	*win_ptr;
@@ -35,5 +37,71 @@ int main()
 	}
 	//mlx_put_image_to_window(mlx_ptr, win_ptr, image.img, 0, 0);
 	mlx_loop(mlx_ptr);
+	return (0);
+}
+
+
+typedef struct	s_vars {
+	void		*mlx;
+	void		*win;
+}				t_vars;
+
+int	key_hook(int keycode, t_vars *vars)
+{
+	static int i = 0;
+	i++;
+	if(keycode == 53)
+	{
+		mlx_destroy_window(vars->mlx, vars->win);
+		exit(0);
+	}
+	vars = 0;
+	printf("%d : Hello from key_hook!\n", i);
+	return (0);
+}
+
+int mouse_hook(int button, int x, int y, void *param)
+{
+	printf("but : %d, x : %d, y : %d, param : %p\n", button, x, y, param);
+	return (0);
+}
+
+int win_close(int button, t_vars *vars)
+{
+	printf("but : %d(%p)\n", button, vars);
+	//mlx_destroy_window(vars->mlx, vars->win);
+	exit(0);
+	return (0);
+}
+
+int mouse_pos(int button, int x, int y, void *param)
+{
+	// if (button == 1)
+		printf("x : %d, y : %d(%d, %p)\n", x, y, button, param);
+	return (0);
+}
+
+int exit_hook()
+{
+	exit(0);
+}
+
+int	hooking(void)
+{
+	t_vars	vars;
+
+	vars.mlx = mlx_init();
+	vars.win = mlx_new_window(vars.mlx, 640, 480, "Hello world!");
+	mlx_key_hook(vars.win, key_hook, &vars);
+	mlx_hook(vars.win, 17, 0, exit_hook, 0);
+	mlx_hook(vars.win, 4, 1L<<2, mouse_hook, 0);
+	mlx_loop(vars.mlx);
+	return (0);
+}
+
+int main()
+{
+	//prtimage();
+	hooking();
 	return (0);
 }
