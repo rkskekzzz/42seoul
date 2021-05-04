460ffbe2f3840f6f1a65e3ca27608f1a2f3fb1a1
su <wkdlfflxh@naver.com>
2021-05-04 13:45:04 +0900

    parse

--- Circle-02/miniRT/parse/parse1.c
+++ Circle-02/miniRT/parse/parse1.c
@@ -44,7 +44,6 @@ int parse_l(char **line, t_box *box)
 	if (ft_arrsize(line) != 4 || !ft_isnum(line[2]))
 		return (ERROR);
 	parsed.l.brightness = ft_atod(line[2]);
-	return (OK);
 	return (parse_split(&(parsed.l.pos), line[1]) && \
 			parse_split(&(parsed.l.color), line[3]) && \
 			parsed_list_add(box, l, parsed));
