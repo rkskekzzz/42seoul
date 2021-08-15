0032ab65862843009ba52b33b09a92f735004f4d
su <wkdlfflxh@naver.com>
2021-08-16 01:35:20 +0900

    philosophers, make routine

--- Circle-03/philosophers/philo/src/core/philo.c
+++ Circle-03/philosophers/philo/src/core/philo.c
@@ -18,7 +18,7 @@ void	init_res(t_resource *res, t_condition *cond)
 
 void	eat(t_philosopher *philo)
 {
-	printf("philo %d is eat\n", philo->idx);
+	printf("philo %d is eat(%d, %d)\n", philo->idx, philo->idx, (philo->idx + 1) % philo->res->cond->num_of_philo);
 	usleep(philo->res->cond->time_to_eat);
 }
 
@@ -27,13 +27,11 @@ void	*routine(void *data)
 	t_philosopher		*philo;
 
 	philo = (t_philosopher *)data;
-	printf("why??\n");
 	pthread_mutex_lock(&philo->res->table);
-	printf("ㅜㅜ\n");
 	pthread_mutex_lock(&philo->res->fork[philo->idx]);
 	pthread_mutex_lock(&philo->res->fork[(philo->idx + 1) % philo->res->cond->num_of_philo]);
 	eat(philo);
-	pthread_mutex_unlock(&philo->res->fork[(philo->idx + 1) % 5]);
+	pthread_mutex_unlock(&philo->res->fork[(philo->idx + 1) % philo->res->cond->num_of_philo]);
 	pthread_mutex_unlock(&philo->res->fork[philo->idx]);
 	pthread_mutex_unlock(&philo->res->table);
 	return (NULL);
@@ -50,9 +48,9 @@ void	philo(t_condition *cond)
 	philo = malloc(sizeof(t_philosopher) * cond->num_of_philo);
 	while (++idx < cond->num_of_philo)
 	{
-		philo->idx = idx;
-		philo->res = &res;
-		pthread_create(&philo->philo, NULL, routine, (void *)&philo);
+		philo[idx].idx = idx;
+		philo[idx].res = &res;
+		pthread_create(&philo->philo, NULL, routine, (void *)&philo[idx]);
 		pthread_join(philo->philo, NULL);
 	}
 	free(philo);
