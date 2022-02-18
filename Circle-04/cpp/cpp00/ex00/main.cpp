d66bf4f3416f935086a0d22aa1556fc0a7e5625e
Suhyoung Shin <suhshin@c4r4s3.42seoul.kr>
2022-02-18 16:11:11 +0900

    ex00

---
+++ Circle-04/cpp/cpp00/ex00/main.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+int main(int argc, char **argv)
+{
+    if (argc == 1)
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+    else {
+        for (int i = 1 ; i < argc ; i++) 
+            
+            for (int j = 0 ; j < std::string(argv[i]).length() ; j++)
+                std::cout << (char)std::toupper(argv[i][j]);
+        std::cout << '\n';
+    }
+    return (0);
+}
