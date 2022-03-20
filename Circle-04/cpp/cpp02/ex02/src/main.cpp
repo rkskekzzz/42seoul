3c8c2467797a6675a3f126cb5e9ade5db96c1a13
rkskekzzz <wkdlfflxh@naver.com>
2022-03-20 20:12:45 +0900

    push

--- Circle-04/cpp/cpp02/ex02/src/main.cpp
+++ Circle-04/cpp/cpp02/ex02/src/main.cpp
@@ -1,9 +1,9 @@
 #include "../include/Fixed.hpp"
 #include <iostream>
 
-int main( void ) {
+int main(void) {
     Fixed a;
-    Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
+    Fixed const b(Fixed(5.05f) * Fixed(2));
 
     std::cout << a << std::endl;
     std::cout << ++a << std::endl;
@@ -11,7 +11,21 @@ int main( void ) {
     std::cout << a++ << std::endl;
     std::cout << a << std::endl;
     std::cout << b << std::endl;
-    std::cout << Fixed::max( a, b ) << std::endl;
+
+    std::cout << (a < b) << std::endl;
+    std::cout << (a > b) << std::endl;
+    std::cout << (a <= b) << std::endl;
+    std::cout << (a >= b) << std::endl;
+    std::cout << (a == b) << std::endl;
+    std::cout << (a != b) << std::endl;
+
+    std::cout << a + b << std::endl;
+    std::cout << a - b << std::endl;
+    std::cout << a / b << std::endl;
+    std::cout << a * b << std::endl;
+
+    std::cout << Fixed::max(a, b) << std::endl;
+    std::cout << Fixed::min(a, b) << std::endl;
 
     return 0;
 }
