6e2ecc39871fa4849d9b965a3d0766232b06c3ee
rkskekzzz <wkdlfflxh@naver.com>
2022-05-08 20:18:08 +0900

    adsf

---
+++ Circle-05/webserv/practice/client.c
@@ -0,0 +1,49 @@
+
+#include <stdio.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <fcntl.h> // open function
+#include <unistd.h> // close function
+#include <strings.h>
+#include <arpa/inet.h>
+
+#define PORT 9000
+#define IPADDR "127.0.0.1"
+
+char buffer[BUFSIZ];
+
+int main() {
+	int c_socket;
+	struct sockaddr_in c_addr;
+	int len;
+
+	char rcvBuffer[BUFSIZ];
+
+	int n;
+
+	c_socket = socket(PF_INET, SOCK_STREAM, 0);
+	memset(&c_addr, 0, sizeof(c_addr));
+
+	c_addr.sin_addr.s_addr = inet_addr(IPADDR);
+	c_addr.sin_family = AF_INET;
+	c_addr.sin_port = htons(PORT);
+
+	if (connect(c_socket, (struct sockaddr *)&c_addr, sizeof(c_addr)) == -1) {
+		printf("Can not connect\n");
+		close(c_socket);
+		return -1;
+	}
+
+	scanf("%s", buffer);
+	buffer[strlen(buffer)] = '\0';
+	write(c_socket, buffer, strlen(buffer) + 1);
+
+	if ((n = read(c_socket, rcvBuffer, sizeof(rcvBuffer))) < 0) {
+		return -1;
+	}
+
+	rcvBuffer[n] = '\0';
+	printf("received Data : %s\n", rcvBuffer);
+
+	close(c_socket);
+}
---
+++ Circle-05/webserv/practice/server.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <fcntl.h> // open function
+#include <unistd.h> // close function
+#include <strings.h>
+#include <arpa/inet.h>
+
+#define PORT 9000
+#define IPADDR "127.0.0.1"
+
+char buffer[BUFSIZ] = "hello, world";
+char rBuffer[BUFSIZ];
+
+int main() {
+	int c_socket, s_socket;
+	struct sockaddr_in s_addr, c_addr;
+	int len;
+
+	char *temp;
+	int length;
+
+	int n, i;
+
+	s_socket = socket(PF_INET, SOCK_STREAM, 0);
+	memset(&s_addr, 0, sizeof(s_addr));
+
+	s_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	s_addr.sin_family = AF_INET;
+	s_addr.sin_port = htons(PORT);
+
+	if (bind(s_socket, (struct sockaddr *)&s_addr, sizeof(s_addr)) == -1) {
+		printf("Can not bind\n");
+		close(s_socket);
+		return -1;
+	}
+
+	if (listen(s_socket, 5) == -1) {
+		printf("listen Fail\n");
+		return -1;
+	}
+
+	while (1) {
+		len = sizeof(c_addr);
+		c_socket = accept(s_socket, (struct sockaddr *)&c_addr, &len);
+
+		length = 0;
+		temp = rBuffer;
+		while((n = read(c_socket, temp, 1)) > 0) {
+			if(*temp == '\r') continue;
+			if(*temp == '\n') break;
+			if(*temp == '\0') break;
+
+			if (length == BUFSIZ) break;
+			temp++;
+			length++;
+		}
+
+		rBuffer[length] = '\0';
+		write(c_socket, rBuffer, strlen(rBuffer));
+		close(c_socket);
+	}
+	close(s_socket);
+}
